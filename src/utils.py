3d996400c5120f8466e375a1534d0e315b9b129a
enesmodalib <168632935+enesmodalib@users.noreply.github.com>
2025-10-06 19:14:12 +0300

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import random, numpy as np, torch
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
